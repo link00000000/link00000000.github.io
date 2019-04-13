3c40568378883d8ef27daabc97533e975dd771c2
link00000000 <crandall.logan@gmail.com>
2019-04-13 00:30:34 -0400

    Added project card change functioning again

--- assets/js/project_card.js
+++ assets/js/project_card.js
@@ -1,9 +1,16 @@
-changeCard = (image, title, desc, link) => {
-    let cardElem = document.querySelector('#projects .card');
-    addClass(cardElem, 'card-transition')
-    cardElem.querySelector('.image').style.backgroundImage = `url(${image})`;
-    cardElem.querySelector('.title').innerHTML = title;
-    cardElem.querySelector('.desc').innerHTML = desc;
-    cardElem.querySelector('.view-on-github').href = link;
-    removeClass(cardElem, 'card-transition');
+changeCard = (thisElem, image, title, desc, link) => {
+    if(!thisElem.classList.contains('current')) {
+        removeClass(document.querySelector('#projects .menu .item.current'), 'current');
+        addClass(thisElem, 'current');
+
+        let cardElem = document.querySelector('#projects .card');
+        addClass(cardElem, 'card-transition');
+        setTimeout(() => {
+            cardElem.querySelector('.image').style.backgroundImage = `url(${image})`;
+            cardElem.querySelector('.title').innerHTML = title;
+            cardElem.querySelector('.desc').innerHTML = desc;
+            cardElem.querySelector('.view-on-github').href = link;
+            removeClass(cardElem, 'card-transition');
+        }, 1000);
+    }
 }
