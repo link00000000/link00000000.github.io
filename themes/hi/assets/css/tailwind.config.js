d903f4b3198ad1d4f90c6fee60fe30ebd12784c2
link00000000 <crandall.logan@gmail.com>
2021-04-29 18:58:50 -0400

    Style first section of homepage

--- themes/hi/assets/css/tailwind.config.js
+++ themes/hi/assets/css/tailwind.config.js
@@ -1,9 +1,12 @@
 module.exports = {
-  theme: {
-    extend: {}
-  },
-  variants: {},
-  plugins: [
-    require('@tailwindcss/typography'),
-  ]
+    theme: {
+        extend: {
+            fontFamily: {
+                heading: ['Montserrat', 'Helvetica', 'Arial', 'sans-serif'],
+                sans: ['Open Sans', 'Helvetica', 'Arial', 'sans-serif']
+            }
+        }
+    },
+    variants: {},
+    plugins: [require('@tailwindcss/typography')]
 }
