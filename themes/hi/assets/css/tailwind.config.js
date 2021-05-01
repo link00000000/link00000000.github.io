18bb42452d4689626f7ce2e230ad493a053bbf4e
link00000000 <crandall.logan@gmail.com>
2021-05-01 17:26:00 -0400

    Style external links

--- themes/hi/assets/css/tailwind.config.js
+++ themes/hi/assets/css/tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
     theme: {
         extend: {
+            colors: {
+                'primary-blue': 'rgb(0, 123, 255)'
+            },
             fontFamily: {
                 heading: ['Montserrat', 'Helvetica', 'Arial', 'sans-serif'],
                 sans: ['Open Sans', 'Helvetica', 'Arial', 'sans-serif']
